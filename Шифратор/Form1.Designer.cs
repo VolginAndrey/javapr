5492aa77130af25a2cd98a6ccab4e72d2fbdd2cf
VolginAndrey <32735454+VolginAndrey@users.noreply.github.com>
2018-05-23 13:12:23 +0600

    Add files via upload

---
+++ Шифратор/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ib_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.key = new System.Windows.Forms.TextBox();
+            this.method = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.encrypted_text = new System.Windows.Forms.TextBox();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.load_ab_from_file = new System.Windows.Forms.Button();
+            this.load_text_from_file = new System.Windows.Forms.Button();
+            this.keylabel = new System.Windows.Forms.Label();
+            this.ab = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input_text
+            // 
+            this.input_text.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input_text.Location = new System.Drawing.Point(25, 221);
+            this.input_text.Multiline = true;
+            this.input_text.Name = "input_text";
+            this.input_text.Size = new System.Drawing.Size(300, 215);
+            this.input_text.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Входящий текст:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Исходный алфавит:";
+            // 
+            // key
+            // 
+            this.key.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.key.Location = new System.Drawing.Point(25, 128);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(300, 20);
+            this.key.TabIndex = 3;
+            // 
+            // method
+            // 
+            this.method.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.method.FormattingEnabled = true;
+            this.method.Items.AddRange(new object[] {
+            "Простая подстановка",
+            "Блочная перестановка",
+            "Усложненная блочная перестановка (змейка)",
+            "Шифр Цезаря",
+            "XOR"});
+            this.method.Location = new System.Drawing.Point(471, 61);
+            this.method.Name = "method";
+            this.method.Size = new System.Drawing.Size(295, 22);
+            this.method.TabIndex = 4;
+            this.method.SelectedIndexChanged += new System.EventHandler(this.method_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(640, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Алгоритм шифрования:";
+            // 
+            // encrypted_text
+            // 
+            this.encrypted_text.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypted_text.Location = new System.Drawing.Point(489, 221);
+            this.encrypted_text.Multiline = true;
+            this.encrypted_text.Name = "encrypted_text";
+            this.encrypted_text.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.encrypted_text.Size = new System.Drawing.Size(300, 215);
+            this.encrypted_text.TabIndex = 6;
+            // 
+            // encrypt
+            // 
+            this.encrypt.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypt.Location = new System.Drawing.Point(331, 221);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(147, 36);
+            this.encrypt.TabIndex = 7;
+            this.encrypt.Text = "Зашифровать";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(331, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // load_ab_from_file
+            // 
+            this.load_ab_from_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load_ab_from_file.Location = new System.Drawing.Point(355, 66);
+            this.load_ab_from_file.Name = "load_ab_from_file";
+            this.load_ab_from_file.Size = new System.Drawing.Size(51, 17);
+            this.load_ab_from_file.TabIndex = 9;
+            this.load_ab_from_file.Text = "Файл...";
+            this.load_ab_from_file.UseVisualStyleBackColor = true;
+            this.load_ab_from_file.Visible = false;
+            this.load_ab_from_file.Click += new System.EventHandler(this.load_ab_from_file_Click);
+            // 
+            // load_text_from_file
+            // 
+            this.load_text_from_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load_text_from_file.Location = new System.Drawing.Point(274, 198);
+            this.load_text_from_file.Name = "load_text_from_file";
+            this.load_text_from_file.Size = new System.Drawing.Size(51, 17);
+            this.load_text_from_file.TabIndex = 10;
+            this.load_text_from_file.Text = "Файл...";
+            this.load_text_from_file.UseVisualStyleBackColor = true;
+            this.load_text_from_file.Visible = false;
+            this.load_text_from_file.Click += new System.EventHandler(this.load_text_from_file_Click);
+            // 
+            // keylabel
+            // 
+            this.keylabel.AutoSize = true;
+            this.keylabel.Location = new System.Drawing.Point(46, 98);
+            this.keylabel.Name = "keylabel";
+            this.keylabel.Size = new System.Drawing.Size(36, 13);
+            this.keylabel.TabIndex = 11;
+            this.keylabel.Text = "Ключ:";
+            this.keylabel.Click += new System.EventHandler(this.keylabel_Click);
+            // 
+            // ab
+            // 
+            this.ab.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ab.Location = new System.Drawing.Point(25, 63);
+            this.ab.Name = "ab";
+            this.ab.Size = new System.Drawing.Size(300, 20);
+            this.ab.TabIndex = 12;
+            this.ab.Text = "abcdefghijklmnopqrstuvwxyz";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(355, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 17);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Файл...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.load_key_from_file_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Зашифрованный текст";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(816, 464);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ab);
+            this.Controls.Add(this.keylabel);
+            this.Controls.Add(this.load_text_from_file);
+            this.Controls.Add(this.load_ab_from_file);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.encrypted_text);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.method);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_text);
+            this.Name = "Form1";
+            this.Text = "Игра";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.ComboBox method;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox encrypted_text;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button load_ab_from_file;
+        private System.Windows.Forms.Button load_text_from_file;
+        private System.Windows.Forms.Label keylabel;
+        private System.Windows.Forms.TextBox ab;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Шифратор/Form1.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace ib_lab
+{
+    public partial class Form1 : Form
+    {
+
+        public class enc
+        {
+
+            string ab, key;
+            public string[] input_text;
+
+            public enc(string a, string k, string[] t) //инициализация класса с нужными значениями
+            {
+                ab = a;
+                key = k;
+                input_text = t;
+            }
+
+            public string XOR(string text, string key)
+            {
+                var result = new StringBuilder();
+                for (int i = 0; i < text.Count(); i++)
+                {
+                    if (text[i] == '0' && key[i % (key.Count() - 1)] == '0')
+                        result.Append('0');
+                    else
+                    if (text[i] == '1' && key[i % (key.Count() - 1)] == '0')
+                        result.Append('1');
+                    else
+                    if (text[i] == '0' && key[i % (key.Count() - 1)] == '1')
+                        result.Append('1');
+                    else
+                    if (text[i] == '1' && key[i % (key.Count() - 1)] == '1')
+                        result.Append('0');
+                    else
+                        break;
+                }
+
+
+                return result.ToString();
+            }
+
+            public string[] cesar()
+            {
+
+                System.Int32 a; //проверка на значение ключа
+                if (!Int32.TryParse(key, out a))
+                {
+                    MessageBox.Show("Для правильной работы этого алгоритма ключ должен быть числовым значением", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                string[] output_text = new string[input_text.Length];
+
+                for (int i = 0; i < input_text.Length; i++)
+                {
+                    string newline = "";
+                    foreach (char sym in input_text[i])
+                    {
+                        int num, int_key = Int32.Parse(key);
+
+                        for (num = 0; num < ab.Length; num++) //поиск символа в алфавите
+                            if (ab[num] == sym) break;
+
+                        if (num + int_key <= ab.Length - 1 && num + int_key >= 0) //если номер символа в границах алфавита
+                            newline += ab[num + int_key];
+
+                        else if (num + int_key < 0) //если номер отрицательный - идти с конца
+                            newline += ab[ab.Length + int_key + num];
+
+                        else //если номер больше размера алфавита - идти с начала
+                            newline += ab[num + int_key - ab.Length];
+
+                        output_text[i] = newline;
+                    }
+                }
+
+                return output_text;
+
+            }
+
+            public string[] replace()
+            {
+                //проверка на соответствие алф. и ключа
+
+                if (ab.Length != key.Length)
+                {
+                    MessageBox.Show("Длина алфавита и длина ключа не совпадает", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                string[] output_text = new string[input_text.Length];
+
+                for (int i = 0; i < input_text.Length; i++)
+                {
+                    string newline = "";
+                    foreach (char sym in input_text[i])
+                        if (ab.IndexOf(sym) == -1) newline += sym;
+                        else newline += key[ab.IndexOf(sym)]; //замена символов в тексте на симв из ключа
+                    output_text[i] = newline;
+                }
+                return output_text;
+            }
+
+            public string[] block_enc()
+            {
+
+                //проверка на значение ключа
+
+                System.Int32 a;
+                if (!Int32.TryParse(key, out a))
+                {
+                    MessageBox.Show("Для правильной работы этого алгоритма ключ должен быть числовым значением", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                string[] output_text = new string[input_text.Length];
+                int i, j;
+
+                //парсим ключ
+
+                int block_size = key.Length;
+                int[] block_order = new int[block_size];
+                for (i = 0; i < block_size; i++)
+                    block_order[i] = (int)Char.GetNumericValue(key[i]);
+
+                //конвертируем весь текст в одну строку
+
+                string tmp_str_input_text = "";
+
+                for (i = 0; i < input_text.Length; i++)
+                    tmp_str_input_text += input_text[i] + ' ';
+
+                int tail_l = tmp_str_input_text.Length % block_size;
+                int enc_part_l = tmp_str_input_text.Length - tail_l;
+
+                char[] str_input_text = new char[enc_part_l];
+                char[] tail = new char[tail_l];
+
+                for (i = 0; i < tmp_str_input_text.Length; i++)
+                    if (i < enc_part_l)
+                        str_input_text[i] = tmp_str_input_text[i];
+                    else
+                        tail[i - enc_part_l] = tmp_str_input_text[i];
+
+                //основной алгоритм
+                if (tmp_str_input_text.Length != block_size + 1)
+                {
+                    MessageBox.Show("Неверный ключ", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                if (str_input_text.Last() == ' ')
+                {
+                    MessageBox.Show("Неверный ключ", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                for (var t = 0; t < block_order.Length; t++)
+                {
+                    if (block_order[t] > str_input_text.Length)
+                    {
+                        MessageBox.Show("Ошибка в ключе", "Ошибка", MessageBoxButtons.OK);
+                        string[] answer = new string[1];
+                        answer[0] = "";
+                        return answer;
+                    }
+                }
+                for (i = 0; i < str_input_text.Length; i += block_size)
+                {
+                    char[] block = new char[block_size]; //массив под текущий блок
+                    char[] temp_block = new char[block_size];
+
+                    for (j = i; j < i + block_size; j++)
+                        block[j - i] = str_input_text[j];
+
+                    for (j = i; j < i + block_size; j++)
+                        temp_block[j - i] = str_input_text[j];
+
+                    for (j = 0; j < block_size; j++)
+                        block[j] = temp_block[block_order[j] - 1];
+
+                    output_text[0] += new string(block);
+                }
+                output_text[0] += new string(tail);
+                return output_text;
+            }
+
+            public string[] hard_block_enc()
+            {
+
+                //проверка на значение ключа
+
+                System.Int32 a;
+                if (!Int32.TryParse(key, out a))
+                {
+                    MessageBox.Show("Для правильной работы этого алгоритма ключ должен быть числовым значением, не превышающим 9", "Ошибка", MessageBoxButtons.OK);
+                    string[] answer = new string[1];
+                    answer[0] = "";
+                    return answer;
+                }
+
+                string[] output_text = new string[512];
+                int i, j;
+
+                //парсим ключ
+
+                int block_size = key.Length;
+                int[] block_order = new int[block_size];
+                for (i = 0; i < block_size; i++)
+                    block_order[i] = (int)Char.GetNumericValue(key[i]);
+
+                //конвертируем весь текст в одну строку
+
+                string tmp_str_input_text = "";
+
+                for (i = 0; i < input_text.Length; i++)
+                    tmp_str_input_text += input_text[i] + ' ';
+
+                int tail_l = tmp_str_input_text.Length % block_size;
+                int enc_part_l = tmp_str_input_text.Length - tail_l;
+
+                char[] str_input_text = new char[enc_part_l];
+                char[] tail = new char[tail_l];
+
+                for (i = 0; i < tmp_str_input_text.Length; i++)
+                    if (i < enc_part_l)
+                        str_input_text[i] = tmp_str_input_text[i];
+                    else
+                        tail[i - enc_part_l] = tmp_str_input_text[i];
+
+                //основной алгоритм
+
+                int k = 0;
+
+                for (i = 0; i < str_input_text.Length; i += block_size)
+                {
+                    char[] block = new char[block_size]; //массив под текущий блок
+                    char[] temp_block = new char[block_size];
+
+                    for (j = i; j < i + block_size; j++)
+                        block[j - i] = str_input_text[j];
+
+                    for (j = i; j < i + block_size; j++)
+                        temp_block[j - i] = str_input_text[j];
+
+                    for (j = 0; j < block_size; j++)
+                        block[j] = temp_block[block_order[j] - 1];
+
+                    if (k % 2 != 0) Array.Reverse(block);
+                    output_text[k] = new string(block);
+                    k++;
+                }
+
+                if (k % 2 != 0) Array.Reverse(tail);
+                output_text[k] = new string(tail); //перевернуть строку слева направо
+                return output_text;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_text_from_file_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                input_text.Lines = File.ReadAllLines(openFileDialog1.FileName);
+            }
+            //string filepath = Interaction.InputBox("Загрузка исходного текста из файла", "Файл:", "");
+            //if (filepath != "")
+            //input_text.Lines = File.ReadAllLines(filepath);
+        }
+
+        private void load_ab_from_file_Click(object sender, EventArgs e)
+        {
+            string filepath = Interaction.InputBox("Загрузка алфавита из файла", "Файл:", "");
+            if (filepath != "" && File.ReadAllLines(filepath).Count() == 1)
+                ab.Lines = File.ReadAllLines(filepath);
+        }
+
+        private void load_key_from_file_Click(object sender, EventArgs e) //загрузка ключа из файла - событие на кнопке
+        {
+            string filepath = Interaction.InputBox("Загрузка ключа из файла", "Файл:", "");
+            if (filepath != "" && File.ReadAllLines(filepath).Count() == 1)
+                key.Lines = File.ReadAllLines(filepath);
+        }
+
+        private void encrypt_Click(object sender, EventArgs e) //клик по кнопке "зашифр"
+        {
+
+            //перевод всего текста в нижний регистр, чтоб не вылетало тк нету нужных симв в алф
+
+            for (int i = 0; i < input_text.Lines.Length; i++) input_text.Lines[i] = input_text.Lines[i].ToLower(); //все в нижний регистр, чтоб не вылетало
+            enc ENC = new enc(ab.Text.ToLower(), key.Text.ToLower(), input_text.Lines);
+            if (input_text.Text != "" && ab.Text != "" && key.Text != "")
+                switch (method.SelectedIndex)
+                {
+                    case 0:
+                        encrypted_text.Lines = ENC.replace();
+                        break;
+
+                    case 1:
+                        encrypted_text.Lines = ENC.block_enc();
+                        break;
+
+                    case 2:
+                        encrypted_text.Lines = ENC.hard_block_enc();
+                        break;
+                    case 3:
+                        encrypted_text.Lines = ENC.cesar();
+                        break;
+                    case 4:
+                        for (var i = 0; i < input_text.Text.Length; i++)
+                        {
+                            if ((input_text.Text[i] > '1') || (input_text.Text[i] < '0'))
+                            {
+                                MessageBox.Show("Некорректный исходный текст", "Ошибка");
+                                return;
+                            }
+
+                        }
+                        for (var i = 0; i < key.Text.Length; i++)
+                        {
+                            if ((Convert.ToInt32(key.Text[i]) > '1') || (Convert.ToInt32(key.Text[i]) < '0'))
+                            {
+                                MessageBox.Show("Некорректный ключ", "Ошибка");
+                                return;
+                            }
+
+                        }
+                        encrypted_text.Text = ENC.XOR(input_text.Text, key.Text);
+                        break;
+                }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //топовая расшифровка
+        {
+            encrypted_text.Lines = input_text.Lines;
+        }
+
+        private void method_SelectedIndexChanged(object sender, EventArgs e) //отключать поле с алфавитом, если выбраны блок, усл блок или xor - там он не нужен
+        {
+            encrypted_text.Text = "";
+            if (
+                method.SelectedIndex == 1 || //блок
+                method.SelectedIndex == 2 || //усл блок
+                method.SelectedIndex == 4    //XOR
+               ) ab.Enabled = false;
+            else ab.Enabled = true;
+
+            key.Text = ""; //сброс значения ключа
+        }
+
+        private void keylabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
